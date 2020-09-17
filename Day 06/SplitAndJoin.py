19e300047f67ba1bf387c016b07f2a695727b65a
divyatejakotteti <56533944+divyatejakotteti@users.noreply.github.com>
2020-09-17 18:08:27 +0530

    Add files via upload

---
+++ Day 06/SplitAndJoin.py
@@ -0,0 +1,24 @@
+'''
+Task
+You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+Input Format
+The first line contains a string consisting of space separated words. 
+#Code:
+
+a=input()
+a=a.split(" ")
+a="-".join(a)
+print(a)
+
+
+'''
+def split_and_join(line):
+    # write your code here
+    return '-'.join(line.split())
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
+'''
---
+++ Day 06/SwapCase.py
@@ -0,0 +1,21 @@
+'''
+You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+Input Format:
+
+A single line containing a string S.
+'''
+#Code:
+
+n=input()
+print(n.swapcase())
+#(or)
+'''def swap_case(s):
+    return s.swapcase()
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+'''
