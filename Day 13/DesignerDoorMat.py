d172b7061cab302ddbe420a4226eb2414e05197b
divyatejakotteti <56533944+divyatejakotteti@users.noreply.github.com>
2020-09-24 17:41:04 +0530

    Add files via upload

---
+++ Day 13/DesignerDoorMat.py
@@ -0,0 +1,66 @@
+'''
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+    Mat size must be NXM. (N is an odd natural number, and M is 3times N.)
+    The design should have 'WELCOME' written in the center.
+    The design pattern should only use |, . and - characters.
+
+Sample Designs
+
+    Size: 7 x 21 
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+    
+    Size: 11 x 33
+    ---------------.|.---------------
+    ------------.|..|..|.------------
+    ---------.|..|..|..|..|.---------
+    ------.|..|..|..|..|..|..|.------
+    ---.|..|..|..|..|..|..|..|..|.---
+    -------------WELCOME-------------
+    ---.|..|..|..|..|..|..|..|..|.---
+    ------.|..|..|..|..|..|..|.------
+    ---------.|..|..|..|..|.---------
+    ------------.|..|..|.------------
+    ---------------.|.---------------
+
+Input Format
+
+A single line containing the space separated values of N and M.
+
+Output Format
+
+Output the design pattern.
+
+Sample Input
+
+9 27
+
+Sample Output
+
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+
+'''
+#Code:
+n,m=map(int,input().split())
+c=".|."
+for i in range(1,n,2):
+    print((c*i).center(m,'-'))
+print('WELCOME'.center(m,'-'))
+for i in range(n-2,-1,-2):
+    print((c*i).center(m,'-'))    
+    
+    
---
+++ Day 13/TextWrap.py
@@ -0,0 +1,43 @@
+'''You are given a string S and width w.
+Your task is to wrap the string into a paragraph of width w.
+
+Input Format
+
+The first line contains a string,S.
+The second line contains the width,w.
+
+
+
+Output Format
+
+Print the text wrapped paragraph.
+
+Sample Input 
+
+ABCDEFGHIJKLIMNOQRSTUVWXYZ
+4
+
+Sample Output 
+
+ABCD
+EFGH
+IJKL
+IMNO
+QRST
+UVWX
+YZ
+
+'''  
+import textwrap
+
+def wrap(string, max_width):
+    a=""
+    for i in range(0,len(string),max_width):
+        a+=string[i:i+max_width]+"\n"
+    return(''.join(a))
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)  
+    
+    
