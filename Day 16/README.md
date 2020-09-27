c8b2da4d69216a9bfba8488510dcde386d88748f
divyatejakotteti <56533944+divyatejakotteti@users.noreply.github.com>
2020-09-27 18:52:53 +0530

    Add files via upload

---
+++ Day 16/README.md
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>#100DaysOfCode Challenge</h1>
+<h1>Day 16: Quick Overview</h1>
+<p>Solved 2 programs from HackerRank - Interview Preparation Kit</p>
+<p>1. <a href="RansomNote.py">RansomNote.py</a></p>
+<p>2. <a href="TwoStrings.py">TwoStrings.py</a></p>
+</body>
+</html>
+<p>Read more about Hackerrank - Interview Preparation kit here : https:<a href="//www.hackerrank.com/interview/interview-preparation-kit">//www.hackerrank.com/interview/interview-preparation-kit</a</p>
---
+++ Day 16/RansomNote.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+   
+# Complete the checkMagazine function below.
+def checkMagazine(magazine, note):
+    for i in magazine:
+        if i in note:
+            magazine.remove(i)
+        else:
+            return False
+    return True
+                  
+if __name__ == '__main__':
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    answer=checkMagazine(magazine, note)
+    if(answer):
+        print("Yes")
+    else:
+        print("No")
+   
+    
+    
