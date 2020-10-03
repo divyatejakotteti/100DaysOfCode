923480227e58c5818115be1da1c419d07dd04197
divyatejakotteti <56533944+divyatejakotteti@users.noreply.github.com>
2020-10-03 16:35:00 +0530

    Add files via upload

---
+++ Day 21/MergeTools.py
@@ -0,0 +1,51 @@
+'''
+Consider the following:
+
+A string, s, of length n where s=c0,c1,c2,...Cn-1.
+An integer,k, where k is a factor of n.
+
+We can split s into n/k subsegments where each subsegment, t, consists of a contiguous block of k characters in s. Then, use each t to create string u such that:
+The characters in u are a subsequence of the characters in t.
+Any repeat occurrence of a character is removed from the string such that each character in u occurs exactly once. In other words, if the character at some index in occurs at a previous index<j in t, then do not include the character in string u.
+
+Given sand k, print n/k lines where each line denotes string u.
+
+Input Format
+
+The first line contains a single string denoting s.
+The second line contains an integer, k, denoting the length of each subsegment.
+Output Format
+
+Print n/k lines where each line i contains string u.
+
+Sample Input
+
+AABCAAADA
+3   
+
+Sample Output
+
+AB
+CA
+AD
+
+'''
+
+def merge_the_tools(string,k):
+    l=[]
+    count=0
+    for i in string:
+        count+=1
+        if i not in l:
+            l.append(i)
+        if count==k:
+            print(''.join(l))
+            l=[]
+            count=0
+
+
+string,k=input(),int(input())
+merge_the_tools(string,k)
+
+   
+    
---
+++ Day 21/README.md
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>#100DaysOfCode Challenge</h1>
+<h1>Day 21:Quick Overview</h1>
+<p>Solved 2 programs from HackerRank - Python</p>
+<p>1. <a href="MergeTools.py">MergeTools.py</a></p>
+<p>2. <a href="IterateTools.py">IterateTools.py</a></p>
+
+</body>
+</html>
