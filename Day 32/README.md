5e85cf7ab6e67511a5c80ad24762e57a0380b344
divyatejakotteti <56533944+divyatejakotteti@users.noreply.github.com>
2020-10-13 17:12:46 +0530

    Add files via upload

---
+++ Day 32/README.md
@@ -0,0 +1,8 @@
+# #100DaysOfCode Challenge
+## Day 32: Quick Overview
+Solved 3 problems from Hackerrank  
+1. [Mark&Toys.py](https://github.com/divyatejakotteti/100DaysOfCode/blob/master/Day%2032/Mark&Toys.py)
+3. [Sorting.py](https://github.com/divyatejakotteti/100DaysOfCode/blob/master/Day%2032/Sorting.py)
+4. [SortingComparator.py](https://github.com/divyatejakotteti/100DaysOfCode/blob/master/Day%2032/SortingComparator.py)
+### 
+Read more about Hackerrank problems bhere : https://www.hackerrank.com/interview/interview-preparation-kit
---
+++ Day 32/Sorting.py
@@ -0,0 +1,102 @@
+'''
+Given an array of integers, sort the array in ascending order using the Bubble Sort algorithm above. Once sorted, print the following three lines:
+
+    Array is sorted in numSwaps swaps., where 
+
+is the number of swaps that took place.
+First Element: firstElement, where
+is the first element in the sorted array.
+Last Element: lastElement, where
+
+    is the last element in the sorted array.
+
+Hint: To complete this challenge, you must add a variable that keeps a running tally of all swaps that occur during execution.
+
+For example, given a worst-case but small array to sort:
+
+we go through the following steps:
+
+swap    a       
+0       [6,4,1]
+1       [4,6,1]
+2       [4,1,6]
+3       [1,4,6]
+
+It took
+
+swaps to sort the array. Output would be
+
+Array is sorted in 3 swaps.  
+First Element: 1  
+Last Element: 6  
+
+Function Description
+
+Complete the function countSwaps in the editor below. It should print the three lines required, then return.
+
+countSwaps has the following parameter(s):
+
+    a: an array of integers .
+
+Input Format
+
+The first line contains an integer,
+, the size of the array .
+The second line contains space-separated integers
+
+.
+
+Constraints
+
+Output Format
+
+You must print the following three lines of output:
+
+    Array is sorted in numSwaps swaps., where 
+
+is the number of swaps that took place.
+First Element: firstElement, where
+is the first element in the sorted array.
+Last Element: lastElement, where
+
+    is the last element in the sorted array.
+
+Sample Input 0
+
+3
+1 2 3
+
+Sample Output 0
+
+Array is sorted in 0 swaps.
+First Element: 1
+Last Element: 3
+
+'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def countSwaps(a):
+    count=0
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if(a[j]>a[j+1]):
+                temp=a[j]
+                a[j]=a[j+1]
+                a[j+1]=temp
+                count+=1
+    print("Array is sorted in " + str(count) + " swaps.")
+    print("First Element: "+ str(a[0]) )
+    print("Last Element: " + str(a[-1]))
+
+if __name__ == '__main__':
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
