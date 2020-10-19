8c0ab3f410861e58fd9fd33fbb0ae1815f3bc15f
divyatejakotteti <56533944+divyatejakotteti@users.noreply.github.com>
2020-10-19 20:43:32 +0530

    Add files via upload

---
+++ Day 38/FairRations.py
@@ -0,0 +1,87 @@
+'''
+You are the benevolent ruler of Rankhacker Castle, and today you're distributing bread. Your subjects are in a line, and some of them already have some loaves. Times are hard and your castle's food stocks are dwindling, so you must distribute as few loaves as possible according to the following rules:
+
+    Every time you give a loaf of bread to some person 
+
+, you must also give a loaf of bread to the person immediately in front of or behind them in the line (i.e., persons or
+
+    ).
+    After all the bread is distributed, each person must have an even number of loaves.
+
+Given the number of loaves already held by each citizen, find and print the minimum number of loaves you must distribute to satisfy the two rules above. If this is not possible, print NO.
+
+For example, the people in line have loaves
+. We can first give a loaf to and so . Next we give a loaf to and and have which satisfies our conditions. We had to distribute
+
+loaves.
+
+Function Description
+
+Complete the fairRations function in the editor below. It should return an integer that represents the minimum number of loaves required.
+
+fairRations has the following parameter(s):
+
+    B: an array of integers that represent the number of loaves each persons starts with .
+
+Input Format
+
+The first line contains an integer
+
+, the number of subjects in the bread line.
+
+The second line contains
+space-separated integers.
+
+Output Format
+
+Print a single integer taht denotes the minimum number of loaves that must be distributed so that every person has an even number of loaves. If it's not possible to do this, print NO.
+
+Sample Input 0
+
+5
+2 3 4 5 6
+
+Sample Output 0
+
+4
+
+'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fairRations function below.
+def fairRations(B):
+    loaves=0
+    for i in range(N-1):
+        if(B[i]%2==0):
+            continue
+        B[i]+=1
+        B[i+1]+=1
+        loaves+=2
+    if(B[-1]%2==1):
+        return("NO")
+    else:
+        return(loaves)
+    
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    N = int(input())
+
+    B = list(map(int, input().rstrip().split()))
+
+    result = fairRations(B)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+  
+    
+    
---
+++ Day 38/README.md
@@ -0,0 +1,6 @@
+# #100DaysOfCode Challenge
+## Day 38: Quick Overview
+Solved 1 problems from Hackerrank  
+1. [FairRations.py](https://github.com/divyatejakotteti/100DaysOfCode/blob/master/Day%2038/FairRations.py)
+### 
+Read more about Hackerrank problems here : https://www.hackerrank.com/domains/algorithms
